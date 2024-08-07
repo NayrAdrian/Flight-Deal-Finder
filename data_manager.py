ad3e4a40a425284e252a7d4d9d48655ee6c05706
Ryan Adrian <125546701+NayrAdrian@users.noreply.github.com>
2024-08-07 15:36:09 +0800

    Added DataManager class to handle fetching and updating destination data from Sheety API. Implemented methods to get destination data and update IATA codes.

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,36 @@
+import os
+import requests
+from requests.auth import HTTPBasicAuth
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/b237e38ca30d773f18ac9108cc89a109/flightDeals/prices"
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+
+    def __init__(self):
+        self._user = os.environ["SHEETY_USERNAME"]
+        self._password = os.environ["SHEETY_PASSWORD"]
+        self._authorization = HTTPBasicAuth(self._user, self._password)
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)
