278a1d44c6a24f26ea80e368c0cb2f270f8ec98c
Ryan Adrian <125546701+NayrAdrian@users.noreply.github.com>
2024-08-12 17:05:29 +0800

    Implemented email notifications to alert customers about flight deals.
    - Updated `.env` file with SMTP settings, including email and app password.
    - Enhanced `NotificationManager` with a `send_emails()` method for sending flight deal notifications.
    - Modified `main.py` to craft email messages tailored to direct and stopover flights.
    - Ensured email encoding to handle special characters like "₱" using "PHP" or UTF-8 encoding.

--- main.py
+++ main.py
@@ -64,10 +64,27 @@ for destination in sheet_data:
         )
         cheapest_flight = find_cheapest_flight(stopover_flights)
         print(f"Cheapest indirect flight price is: ₱{cheapest_flight.price}")
-        notification_manager.send_whatsapp(
-            message_body=f"Low price alert! Only ₱{cheapest_flight.price} to fly "
-                         f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
-                         f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
-        )
+
+    # ==================== Send Notifications and Emails  ====================
+
+    if cheapest_flight.price != "N/A" and cheapest_flight.price < destination["lowestPrice"]:
+        # Customize the message depending on the number of stops
+        if cheapest_flight.stops == 0:
+            message = f"Low price alert! Only PHP {cheapest_flight.price} to fly direct "\
+                      f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "\
+                      f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
+        else:
+            message = f"Low price alert! Only PHP {cheapest_flight.price} to fly "\
+                      f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "\
+                      f"with {cheapest_flight.stops} stop(s) "\
+                      f"departing on {cheapest_flight.out_date} and returning on {cheapest_flight.return_date}."
+
+        print(f"Check your email. Lower price flight found to {destination['city']}!")
+
+        # notification_manager.send_sms(message_body=message)
+        notification_manager.send_whatsapp(message_body=message)
+
+        # Send emails to everyone on the list
+        notification_manager.send_emails(email_list=customer_email_list, email_body=message)
 
 
